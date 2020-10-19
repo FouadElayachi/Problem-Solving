37f35c8e9fdb6ab1b03928874c9fe0c12dddc140
FouadElayachi <elayachifouad@gmail.com>
2020-10-19 17:09:19 +0100

    Improve multiple of 3 and 5 solutions to the euler project.

--- Hackerrank/Project Euler/#1: Multiples of 3 and 5/Solution.java
+++ Hackerrank/Project Euler/#1: Multiples of 3 and 5/Solution.java
@@ -10,12 +10,15 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         for(int a0 = 0; a0 < t; a0++){
-            int n = in.nextInt();
-            int sum = 0;
-            for(int i = 0; i < n; i++) {
-                if(i % 3 == 0 || i % 5 == 0)
-                    sum += i;
-            }
+            long n = in.nextInt();
+            long a = 0, b = 0, c = 0;
+            a = (n - 1) / 3;
+            b = (n - 1) / 5;
+            c = (n - 1) / 15;
+            long sum3 = 3 * a * (a + 1) / 2;
+            long sum5 = 5 * b * (b + 1) / 2;
+            long sum15 = 15 * c * (c + 1) / 2;
+            long sum = sum3 + sum5 - sum15;
             System.out.println(sum);
         }
     }

0e7dd318faa30fd0fd12da58595f68aafcc703fb
FouadElayachi <elayachifouad@gmail.com>
2020-10-19 02:38:46 +0100

    Add Divisible Bill Division from hackerrank.

---
+++ Hackerrank/2-Bill Division/Solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<Integer> bill, int k, int b) {
+        long annaPay = 0;
+        long sumK = 0;
+        for(int i = 0; i < bill.size(); i++) {
+            if(i == k) {
+                continue;
+            }
+            sumK += bill.get(i);
+        }
+        annaPay = sumK / 2;
+        if(annaPay == b) {
+            System.out.println("Bon Appetit");
+        }
+        else {
+            System.out.println(Math.abs(b - annaPay));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nk = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        String[] billItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> bill = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int billItem = Integer.parseInt(billItems[i]);
+            bill.add(billItem);
+        }
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
